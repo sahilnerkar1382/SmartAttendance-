f5b155628d6edf38dc41716da0bda64687255403
Sahil Nerkar <sahilnerkar0@gmail.com>
2026-06-18 21:08:44 +0530

    Initial clean commit

---
+++ src/components/footer.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+
+def footer_home():
+    logo_url = ""
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:white;"> Created with ❤️ by Sahil </p>  
+        <img src='{logo_url}' style='max-height:25px' />
+        </div>
+                
+                """, unsafe_allow_html=True)
+
+
+def footer_dashboard():
+    logo_url = ""
+    
+    st.markdown(f"""
+        <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
+        <p style="font-weight:bold; color:black;"> Created with ❤️ by  Sahil</p>  
+        <img src='{logo_url}' style='max-height:25px' />
+        </div>
+                
+                """, unsafe_allow_html=True)
