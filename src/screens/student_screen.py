d522fe7a6b5991f95df2790a369e73c49a112f68
Sahil Nerkar <sahilnerkar0@gmail.com>
2026-06-23 12:26:52 +0530

    Added supabase dependency

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -133,7 +133,7 @@ def student_screen():
                         st.session_state.is_logged_in = True
                         st.session_state.user_role = 'student'
                         st.session_state.student_data = student
-                        st.toast(f'Welcome Back {student['name']}')
+                        st.toast(f'Welcome Back {student["name"]}')
                         time.sleep(1)
                         st.rerun()
                 else:
